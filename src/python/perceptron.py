a7e6638dc727c7f38efe52ef6fec81fdd3d7afa6
William Emfinger <waemfinger@gmail.com>
2017-02-24 14:28:13 -0600

    added plottinc capability for showing decision region/surface.

--- src/python/perceptron.py
+++ src/python/perceptron.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 class Perceptron(object):
     """Perceptron Classifier.
@@ -63,6 +64,29 @@ class Perceptron(object):
         """Return class label after unit step."""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
 
+def plot_decision_regions(x, y, classifier, resolution=0.02):
+    # set up marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = x[:, 0].min() - 1, x[:, 0].max() + 1
+    x2_min, x2_max = x[:, 1].min() - 1, x[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    #plot class samples
+    for idx, c1 in enumerate(np.unique(y)):
+        plt.scatter(x=x[y==c1, 0], y=x[y==c1, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=c1)
+
 def main(argv):
     df = pd.read_csv(
         'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
@@ -87,6 +111,12 @@ def main(argv):
     plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
     plt.xlabel('Epochs')
     plt.ylabel('Number of misclassifications')
+    plt.show()
+
+    plot_decision_regions(x, y, classifier=ppn)
+    plt.xlabel('sepal length [cm]')
+    plt.ylabel('petal length [cm]')
+    plt.legend(loc='upper left')
     plt.show()
 
 if __name__ == "__main__":
